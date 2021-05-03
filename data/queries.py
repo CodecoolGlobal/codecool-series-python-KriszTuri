12e2fb1d37a448065c1d0237a0aaf233932e8ad6
KriszTuri <turi.krisztina1999@gmail.com>
2021-05-03 20:11:18 +0200

    Pagination

--- data/queries.py
+++ data/queries.py
@@ -10,4 +10,12 @@ def get_most_rated():
                                            JOIN show_genres ON show_genres.show_id = shows.id
                                            JOIN genres ON genres.id = show_genres.genre_id
                                            GROUP BY(shows.id)
-                                           ORDER BY rating DESC LIMIT 15;''')
+                                           ORDER BY rating DESC;''')
+
+def get_shows_count():
+    return data_manager.execute_select(f'''SELECT COUNT(*) FROM(SELECT shows.id, shows.title, shows.year, shows.runtime, ROUND(shows.rating,2) AS rating, shows.trailer, shows.homepage,STRING_AGG(genres.name,', ') AS name 
+                                           FROM shows 
+                                           JOIN show_genres ON show_genres.show_id = shows.id
+                                           JOIN genres ON genres.id = show_genres.genre_id
+                                           GROUP BY(shows.id)
+                                           ORDER BY rating DESC) AS something;''')
--- main.py
+++ main.py
@@ -18,9 +18,29 @@ def design():
 
 @app.route('/shows/most-rated')
 def most_rated():
+    start = 0
+    end = 15
+    current_page = int(start)
+    next_page = current_page+1
     most_rated_shows = queries.get_most_rated()
-    print(most_rated_shows)
-    return render_template('most_rated_shows.html', most_rated_shows = most_rated_shows)
+    show_count = queries.get_shows_count()
+    
+    page_count = math.floor(show_count[0]['count']/16)
+    print(page_count)
+    return render_template('most_rated_shows.html', most_rated_shows = most_rated_shows, page_count = page_count, start = start, end = end, current_page=current_page, next_page=next_page)
+
+@app.route('/shows/most-rated/<pgnum>')
+def test(pgnum):
+    start = 16*int(pgnum)-1
+    end = start+15
+    current_page = int(pgnum)
+    next_page = current_page+1
+    prev_page = current_page-1
+    most_rated_shows = queries.get_most_rated()
+    show_count = queries.get_shows_count()
+    page_count = math.floor(show_count[0]['count']/16)
+    return render_template('most_rated_shows.html', most_rated_shows = most_rated_shows, page_count = page_count, start = start, end = end, current_page=current_page, next_page=next_page, prev_page=prev_page)
+
 
 
 def main():
